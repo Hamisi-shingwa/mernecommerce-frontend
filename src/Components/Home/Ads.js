ae7d774ebc69f835c2fbb85ca9f791f499d5aed6
Hamisi Faraji Hamisi <hamisifaraji369@gmail.com>
2022-12-29 09:09:49 -0400

    change home layout

--- src/Components/Home/Ads.js
+++ src/Components/Home/Ads.js
@@ -6,7 +6,7 @@ export default function Ads() {
     <div className='parent-add-container'>
          <div className='top-user-ads ads-header'>Availabe Trending adds</div>
         <div className='user-products'>
-        <div className='ads-container'>
+    <div className='ads-container'>
        <div className='product-image'><img src={men} alt=''/></div>
        <div className='product-location rounded-for-item-legth'>{`1`}</div>
        <div className='product-name-time'>
@@ -110,6 +110,36 @@ export default function Ads() {
        </div>
       
 
+       <div className='ads-container'>
+       <div className='product-image'><img src={men} alt=''/></div>
+       <div className='product-location rounded-for-item-legth'>{`1`}</div>
+       <div className='product-name-time'>
+       <div className='product-name'>Lenoval-Thinkpad</div>
+       <div className='uploaded-on'>{`3 min`}</div>
+       </div>
+       <div className='product-price'>
+           <div className='price'>Price</div>
+           <div className='amount'>40000k</div>
+           </div>
+       {/* <button className='jumbotron_button'>Shop now</button> */}
+       </div>
+
+       
+       <div className='ads-container'>
+       <div className='product-image'><img src={men} alt=''/></div>
+       <div className='product-location rounded-for-item-legth'>{`1`}</div>
+       <div className='product-name-time'>
+       <div className='product-name'>Lenoval-Thinkpad</div>
+       <div className='uploaded-on'>{`3 min`}</div>
+       </div>
+       <div className='product-price'>
+           <div className='price'>Price</div>
+           <div className='amount'>40000k</div>
+           </div>
+       {/* <button className='jumbotron_button'>Shop now</button> */}
+       </div>
+
+       
        <div className='ads-container'>
        <div className='product-image'><img src={men} alt=''/></div>
        <div className='product-location rounded-for-item-legth'>{`1`}</div>
